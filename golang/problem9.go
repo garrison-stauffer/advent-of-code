2be3febf08eb9ef0d5e7963f4ffb763f8aabb356
Garrison Stauffer <gar.stauffer@gmail.com>
2025-12-14 02:10:12 -0500

    problem 9 part 2, very slow solution but it works

--- golang/problem9.go
+++ golang/problem9.go
@@ -10,182 +10,306 @@ import (
 	"strings"
 )
 
-type Problem8 struct {
-	TopN int
-}
+type Problem9 struct{}
 
-func (p *Problem8) Part1(input []string) (int, error) {
+func (p *Problem9) Part1(input []string) (int, error) {
 	type coordinate struct {
-		x, y, z int
+		x, y int
 	}
-	var coordinates []coordinate
-	for _, line := range input {
-		rawCoords := strings.Split(line, ",")
-		coordinates = append(coordinates, coordinate{
-			x: util.Must(strconv.Atoi(rawCoords[0])),
-			y: util.Must(strconv.Atoi(rawCoords[1])),
-			z: util.Must(strconv.Atoi(rawCoords[2])),
-		})
+	coordinates := make([]coordinate, len(input))
+	for i, line := range input {
+		x := util.Must(strconv.Atoi(strings.Split(line, ",")[0]))
+		y := util.Must(strconv.Atoi(strings.Split(line, ",")[1]))
+		coordinates[i] = coordinate{x, y}
 	}
 
-	type pair struct {
-		distance float64
-		aIdx     int
-		a        string
-		bIdx     int
-		b        string
-	}
-	var distances []pair
+	largest := -1
 	for aIdx, a := range coordinates {
 		for bIdx, b := range coordinates {
 			if aIdx <= bIdx {
 				continue
 			}
-			x2 := math.Pow(float64(a.x-b.x), 2)
-			y2 := math.Pow(float64(a.y-b.y), 2)
-			z2 := math.Pow(float64(a.z-b.z), 2)
-			distance := math.Sqrt(x2 + y2 + z2)
-			distances = append(distances, pair{
-				distance: distance,
-				aIdx:     aIdx,
-				a:        input[aIdx],
-				bIdx:     bIdx,
-				b:        input[bIdx],
-			})
+
+			// are there any coordinates that would block this?
+
+			width := max(a.x, b.x) - min(a.x, b.x) + 1
+			height := max(a.y, b.y) - min(a.y, b.y) + 1
+			largest = max(largest, width*height)
 		}
 	}
 
-	slices.SortFunc(distances, flnt.SortBy(func(pair pair) float64 {
-		return pair.distance
-	}))
-
-	circuitMembers := make(map[int][]int)
-	circuits := make([]int, len(input))
-	for i := range circuits {
-		circuits[i] = i
-		circuitMembers[i] = []int{i}
-	}
-
-	slog.Debug("distances", "distances", distances)
-
-	// [0 1 2 3 4 5 6 7 8 9]
-	// [  ^         ^      ] // merge circuits 6 and 1 into 1
-	// [0 1 2 3 4 5 1 7 8 9]
-	// [^           ^      ] // merge circuits 0 and 1 into 0
-	// [0 0 2 3 4 5 0 7 8 9]
-	//
-	for i := range p.TopN {
-		pair := distances[i]
-		aCircuit := circuits[pair.aIdx]
-		bCircuit := circuits[pair.bIdx]
-		slog.Debug("merging these", "a", input[pair.aIdx], "b", input[pair.bIdx], "aCircuit", aCircuit, "bCircuit", bCircuit)
-		if aCircuit == bCircuit {
-			continue
-		}
-
-		// merge the circuits now
-		mergingInto := min(aCircuit, bCircuit)
-		mergingOutOf := max(aCircuit, bCircuit)
-
-		circuitMembers[mergingInto] = append(circuitMembers[mergingInto], circuitMembers[mergingOutOf]...)
-		for _, nodeId := range circuitMembers[mergingOutOf] {
-			circuits[nodeId] = mergingInto
-		}
-		delete(circuitMembers, mergingOutOf)
-	}
-
-	var lens []int
-	for _, members := range circuitMembers {
-		lens = append(lens, len(members))
-	}
-	slices.Sort(lens)
-	slices.Reverse(lens)
-
-	return lens[0] * lens[1] * lens[2], nil
+	return largest, nil
 }
 
-func (p *Problem8) Part2(input []string) (int, error) {
+func (p *Problem9) Part2(input []string) (int, error) {
 	type coordinate struct {
-		x, y, z int
+		x, y int
 	}
-	var coordinates []coordinate
-	for _, line := range input {
-		rawCoords := strings.Split(line, ",")
-		coordinates = append(coordinates, coordinate{
-			x: util.Must(strconv.Atoi(rawCoords[0])),
-			y: util.Must(strconv.Atoi(rawCoords[1])),
-			z: util.Must(strconv.Atoi(rawCoords[2])),
-		})
+	type lines struct {
+		a, b       coordinate
+		aIdx, bIdx int
+		idx        int
+	}
+	coordinates := make([]coordinate, len(input))
+	for i, line := range input {
+		x := util.Must(strconv.Atoi(strings.Split(line, ",")[0]))
+		y := util.Must(strconv.Atoi(strings.Split(line, ",")[1]))
+		coordinates[i] = coordinate{x, y}
 	}
 
-	type pair struct {
-		distance float64
-		aIdx     int
-		a        coordinate
-		bIdx     int
-		b        coordinate
+	connections := make([]lines, len(input))
+	for i := range coordinates {
+		aIdx := i
+		bIdx := (i + 1) % len(input)
+		a := coordinates[aIdx]
+		b := coordinates[bIdx]
+		connections[i] = lines{a, b, aIdx, bIdx, i}
 	}
-	var distances []pair
+
+	// ..............
+	// .......#XXX#..
+	// .......X...X..
+	// ..#XXXX#...X..
+	// ..X........X..
+	// ..#XXXXXX#.X..
+	// .........X.X..
+	// .........#X#..
+	// ..............
+
+	findOverlappingForX := func(x int) []lines {
+		result := []lines{}
+		for _, line := range connections {
+			x1 := min(line.a.x, line.b.x)
+			x2 := max(line.a.x, line.b.x)
+
+			if x1 <= x && x2 >= x {
+				result = append(result, line)
+			}
+		}
+		return result
+	}
+
+	var getRangesOverXCache = make(map[int][]struct {
+		begin, end int
+		isTiled    bool
+	})
+	getRangesOverX := func(x int) []struct {
+		begin, end int
+		isTiled    bool
+	} {
+		if known, ok := getRangesOverXCache[x]; ok {
+			return known
+		}
+
+		overlappingRanges := findOverlappingForX(x)
+		slices.SortFunc(overlappingRanges, flnt.SortBy(func(line lines) int {
+			return min(line.a.y, line.b.y)
+		}))
+
+		var rangesForX []struct {
+			begin, end int
+			isTiled    bool
+		}
+		rangeStartY := 0
+		isInside := false
+
+		for i := 0; i < len(overlappingRanges); i++ {
+			// okay now we apply our ugly logic...
+			// is this horizontal or vertical
+			line := overlappingRanges[i]
+			isVertical := line.a.x == line.b.x
+
+			if isVertical {
+				// on the border of length of map it gets awkward
+				var previousIdx, nextIdx int
+				if max(line.aIdx, line.bIdx)-min(line.aIdx, line.bIdx) > 1 {
+					// previous is actually 499 since we are crossing modulo point
+					previousIdx = (max(line.aIdx, line.bIdx) - 1 + len(coordinates)) % len(coordinates)
+					nextIdx = (min(line.aIdx, line.bIdx) + 1 + len(coordinates)) % len(coordinates)
+				} else {
+					previousIdx = (min(line.aIdx, line.bIdx) - 1 + len(coordinates)) % len(coordinates)
+					nextIdx = (max(line.aIdx, line.bIdx) + 1 + len(coordinates)) % len(coordinates)
+				}
+				previous := coordinates[previousIdx]
+				next := coordinates[nextIdx]
+				// if they go the same way then we stay at what ever our previous state was (we rode an edge, but did not cross a border)
+				goesAllLeft := previous.x < line.a.x && next.x < line.a.x
+				goesAllRight := previous.x > line.a.x && next.x > line.a.x
+				isBorder := !goesAllRight && !goesAllLeft
+
+				if !isBorder && !isInside {
+					// add 2 ranges - one for the blank area, and one for the valid area
+					// does our vertical line start at the exact edge? then no empty range!
+					hasEmptyRange := rangeStartY != min(line.a.y, line.b.y)
+					if hasEmptyRange {
+						rangesForX = append(rangesForX, struct {
+							begin, end int
+							isTiled    bool
+						}{begin: rangeStartY, end: min(line.a.y, line.b.y) - 1, isTiled: false})
+					}
+					rangesForX = append(rangesForX, struct {
+						begin, end int
+						isTiled    bool
+					}{begin: min(line.a.y, line.b.y), end: max(line.a.y, line.b.y), isTiled: true})
+
+					rangeStartY = max(line.a.y, line.b.y) + 1
+				} else if isBorder {
+					// it's a border, we either finish recording a empty region or we
+					if isInside {
+						rangesForX = append(rangesForX, struct {
+							begin, end int
+							isTiled    bool
+						}{begin: rangeStartY, end: max(line.a.y, line.b.y), isTiled: true})
+						rangeStartY = max(line.a.y, line.b.y) + 1
+						isInside = false
+					} else {
+						// outside
+						hasEmptyRange := rangeStartY != min(line.a.y, line.b.y)
+						if hasEmptyRange {
+							rangesForX = append(rangesForX, struct {
+								begin, end int
+								isTiled    bool
+							}{begin: rangeStartY, end: min(line.a.y, line.b.y) - 1, isTiled: false})
+						}
+						rangeStartY = min(line.a.y, line.b.y)
+						isInside = true
+					}
+				}
+			} else {
+				// it's horizontal
+				// if its vertice lands on our column, skip processing
+				if line.a.x == x || line.b.x == x {
+					continue
+				}
+
+				if !isInside {
+					// I should be able to just skip vertex hits, they will always be covered by another
+					// I increase at the end of the vertical for the next "zone, but actually it is continued here; maybe I can detect if range start is y+1
+					// nothing really to do here, we ended on the vertex so it should be fine
+					hasEmptyRange := rangeStartY != line.a.y
+
+					if hasEmptyRange {
+						rangesForX = append(rangesForX, struct {
+							begin, end int
+							isTiled    bool
+						}{begin: rangeStartY, end: min(line.a.y, line.b.y) - 1, isTiled: false})
+					}
+					rangeStartY = line.a.y
+					isInside = true
+				} else {
+					rangesForX = append(rangesForX, struct {
+						begin, end int
+						isTiled    bool
+					}{begin: rangeStartY, end: line.a.y, isTiled: true})
+					rangeStartY = line.a.y + 1
+					isInside = false
+				}
+			}
+		}
+		rangesForX = append(rangesForX, struct {
+			begin, end int
+			isTiled    bool
+		}{begin: rangeStartY, end: math.MaxInt, isTiled: isInside})
+
+		getRangesOverXCache[x] = rangesForX
+		return rangesForX
+	}
+	//
+	//minX := math.MaxInt
+	//maxX := math.MinInt
+	//for i := 0; i < len(coordinates)-1; i++ {
+	//	minX = min(minX, coordinates[i].x)
+	//	minX = min(minX, coordinates[i+1].x)
+	//	maxX = max(maxX, coordinates[i].x)
+	//	maxX = max(maxX, coordinates[i+1].x)
+	//	ranges := getRangesOverX(coordinates[i].x)
+	//
+	//	for i := 0; i < len(ranges)-1; i++ {
+	//		curr := ranges[i]
+	//		next := ranges[i+1]
+	//		if curr.begin > curr.end {
+	//			slog.Debug("current range has higher beginning than end", "x", coordinates[i].x, "current range", curr)
+	//		}
+	//		if curr.end+1 != next.begin {
+	//			slog.Debug("current range end does not match next range begin", "x", coordinates[i].x, "current range", curr, "next", next)
+	//		}
+	//	}
+	//
+	//	if ranges[len(ranges)-1].end != math.MaxInt {
+	//		slog.Debug("end of range isn't max int", "x", coordinates[i].x, "range", ranges[len(ranges)-1])
+	//	}
+	//}
+	//for i := minX; i <= maxX; i++ {
+	//	ranges := getRangesOverX(i)
+	//
+	//	for i := 0; i < len(ranges)-1; i++ {
+	//		curr := ranges[i]
+	//		next := ranges[i+1]
+	//		if curr.begin > curr.end {
+	//			slog.Debug("current range has higher beginning than end", "x", coordinates[i].x, "current range", curr)
+	//		}
+	//		if curr.end+1 != next.begin {
+	//			slog.Debug("current range end does not match next range begin", "x", coordinates[i].x, "current range", curr, "next", next)
+	//		}
+	//	}
+	//
+	//	if ranges[len(ranges)-1].end != math.MaxInt {
+	//		slog.Debug("end of range isn't max int", "x", coordinates[i].x, "range", ranges[len(ranges)-1])
+	//	}
+	//}
+
+	// consider each pair again
+
+	largest := -1
 	for aIdx, a := range coordinates {
 		for bIdx, b := range coordinates {
 			if aIdx <= bIdx {
 				continue
 			}
-			x2 := math.Pow(float64(a.x-b.x), 2)
-			y2 := math.Pow(float64(a.y-b.y), 2)
-			z2 := math.Pow(float64(a.z-b.z), 2)
-			distance := math.Sqrt(x2 + y2 + z2)
-			distances = append(distances, pair{
-				distance: distance,
-				aIdx:     aIdx,
-				a:        a,
-				bIdx:     bIdx,
-				b:        b,
-			})
+
+			// assemble the rectangle
+			x1 := min(a.x, b.x)
+			x2 := max(a.x, b.x)
+			y1 := min(a.y, b.y)
+			y2 := max(a.y, b.y)
+			isValidPair := true
+
+			for x := x1; x <= x2; x++ {
+				ranges := getRangesOverX(x)
+				used := []struct {
+					begin, end int
+					isTiled    bool
+				}{}
+				isValid := true
+				for _, chunk := range ranges {
+					hasOverlap := y1 <= chunk.end && y2 >= chunk.begin
+					if hasOverlap {
+						//slog.Debug("found some overlap between these", "y1", y1, "y2", y2, "chunk", chunk, "y1 <= chunk.end", y1 <= chunk.end, "y2 >= chunk.begin", y2 >= chunk.begin)
+						isValid = isValid && chunk.isTiled
+						used = append(used, chunk)
+					}
+				}
+
+				if !isValid {
+					slog.Debug("invalid block", "a", a, "b", b, "x", x, "used", used)
+					isValidPair = false
+					break
+				} else {
+					slog.Debug("valid but should it?", "a", a, "b", b, "x", x, "used", used)
+				}
+			}
+
+			if isValidPair {
+				width := x2 - x1 + 1
+				height := y2 - y1 + 1
+				largest = max(largest, width*height)
+				if largest == width*height {
+					slog.Info("new max biggest at", "a", a, "b", b)
+				}
+			}
+
 		}
 	}
 
-	slices.SortFunc(distances, flnt.SortBy(func(pair pair) float64 {
-		return pair.distance
-	}))
-
-	circuitMembers := make(map[int][]int)
-	circuits := make([]int, len(input))
-	for i := range circuits {
-		circuits[i] = i
-		circuitMembers[i] = []int{i}
-	}
-
-	slog.Debug("distances", "distances", distances)
-
-	// [0 1 2 3 4 5 6 7 8 9]
-	// [  ^         ^      ] // merge circuits 6 and 1 into 1
-	// [0 1 2 3 4 5 1 7 8 9]
-	// [^           ^      ] // merge circuits 0 and 1 into 0
-	// [0 0 2 3 4 5 0 7 8 9]
-	//
-	for _, pair := range distances {
-		aCircuit := circuits[pair.aIdx]
-		bCircuit := circuits[pair.bIdx]
-		slog.Debug("merging these", "a", input[pair.aIdx], "b", input[pair.bIdx], "aCircuit", aCircuit, "bCircuit", bCircuit)
-		if aCircuit == bCircuit {
-			continue
-		}
-
-		// merge the circuits now
-		mergingInto := min(aCircuit, bCircuit)
-		mergingOutOf := max(aCircuit, bCircuit)
-
-		circuitMembers[mergingInto] = append(circuitMembers[mergingInto], circuitMembers[mergingOutOf]...)
-		for _, nodeId := range circuitMembers[mergingOutOf] {
-			circuits[nodeId] = mergingInto
-		}
-		delete(circuitMembers, mergingOutOf)
-
-		if len(circuitMembers) == 1 {
-			return pair.a.x * pair.b.x, nil
-		}
-	}
-
-	return -1, nil
+	return largest, nil
 }
--- golang/problem9_test.go
+++ golang/problem9_test.go
@@ -3,17 +3,20 @@ package golang
 import (
 	"fmt"
 	"garrison-stauffer.com/advent-of-code/flnt"
+	"garrison-stauffer.com/advent-of-code/util"
 	"github.com/stretchr/testify/require"
+	"strconv"
+	"strings"
 	"testing"
 )
 
-func TestProblem8(t *testing.T) {
-	prob := Problem8{}
+func TestProblem9(t *testing.T) {
+	prob := Problem9{}
 
 	t.Run("run the problem", func(t *testing.T) {
-		prob.TopN = 1000
-		result, err := prob.Part1(ReadProblemInput(8, 1))
-		result2, err2 := prob.Part2(ReadProblemInput(8, 1))
+		//setDebugger(true)
+		result, err := prob.Part1(ReadProblemInput(9, 1))
+		result2, err2 := prob.Part2(ReadProblemInput(9, 1))
 
 		require.NoError(t, err)
 		require.NoError(t, err2)
@@ -22,32 +25,19 @@ func TestProblem8(t *testing.T) {
 	})
 
 	t.Run("part 1 test cases", func(t *testing.T) {
-		prob.TopN = 10
 		do := Suite(prob.Part1)
 
 		t.Run("provided test case", func(t *testing.T) {
 			do(t).debug().parsing(flnt.Spl(
-				`162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689`,
-			)).hasToBe(40)
+				`7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`,
+			)).hasToBe(50)
 		})
 	})
 	t.Run("part 2 test cases", func(t *testing.T) {
@@ -55,28 +45,88 @@ func TestProblem8(t *testing.T) {
 		do := Suite(prob.Part2)
 
 		t.Run("provided test cases", func(t *testing.T) {
-			do(t).parsing(flnt.Spl(
-				`162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689`,
-			)).hasToBe(21)
+			do(t).debug().parsing(flnt.Spl(
+				`7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`,
+			)).hasToBe(24)
 		})
+		t.Run("provided test cases", func(t *testing.T) {
+			do(t).debug().parsing(flnt.Spl(
+				`1,2
+1,7
+3,7
+3,9
+4,9
+4,5
+5,5
+5,9
+6,9
+6,5
+7,5
+7,9
+8,9
+8,2	`,
+			)).hasToBe(36)
+		})
+	})
+
+	t.Run("explore the data", func(t *testing.T) {
+
+		input := ReadProblemInput(9, 1)
+		type coordinate struct {
+			x, y int
+		}
+		coordinates := make([]coordinate, len(input))
+		for i, line := range input {
+			x := util.Must(strconv.Atoi(strings.Split(line, ",")[0]))
+			y := util.Must(strconv.Atoi(strings.Split(line, ",")[1]))
+			coordinates[i] = coordinate{x, y}
+		}
+
+		for i := range coordinates {
+			a := coordinates[i]
+			b := coordinates[(i+1)%len(coordinates)]
+			c := coordinates[(i+2)%len(coordinates)]
+
+			if a.x == b.x && a.x == c.x {
+				panic("yes there is a chained vertical")
+			}
+			if a.y == b.y && a.y == c.y {
+				panic("yes there is a chained horizontal")
+			}
+		}
+	})
+
+	t.Run("explore what my results are", func(t *testing.T) {
+
+		input := ReadProblemInput(9, 1)
+		type coordinate struct {
+			x, y int
+		}
+		coordinates := make([]coordinate, len(input))
+		for i, line := range input {
+			x := util.Must(strconv.Atoi(strings.Split(line, ",")[0]))
+			y := util.Must(strconv.Atoi(strings.Split(line, ",")[1]))
+			coordinates[i] = coordinate{x, y}
+		}
+
+		for i := range coordinates {
+			a := coordinates[i]
+			b := coordinates[(i+1)%len(coordinates)]
+			c := coordinates[(i+2)%len(coordinates)]
+
+			if a.x == b.x && a.x == c.x {
+				panic("yes there is a chained vertical")
+			}
+			if a.y == b.y && a.y == c.y {
+				panic("yes there is a chained horizontal")
+			}
+		}
 	})
 }
